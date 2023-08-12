6d6ec7cccf551837819712d344c87344e6a67f78
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-08-11 22:39:39 -0600

    Build reusable Heading component with conditional styles

---
+++ src/ui/Heading.tsx
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+interface HeadingProps {
+  as: "h1" | "h2" | "h3";
+}
+
+const Heading = styled.h1<HeadingProps>`
+  ${({ as }) =>
+    as === "h1" &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+  ${({ as }) =>
+    as === "h2" &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+  ${({ as }) =>
+    as === "h3" &&
+    css`
+      font-size: 2rem;
+      font-weight: 500;
+    `}
+`;
+
+export default Heading;
