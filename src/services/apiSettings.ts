51dd8d60971d4950667d71ea83d659fdf1b48743
Maxwell Rafuse <maxwell.rafuse@proton.me>
2023-08-27 14:07:04 -0600

    Handle case conversions for settings data

--- src/services/apiSettings.ts
+++ src/services/apiSettings.ts
@@ -1,6 +1,8 @@
 import supabase from "@/services/supabase";
 
-import type { PostgrestError } from "@supabase/supabase-js";
+import { PostgrestError } from "@supabase/supabase-js";
+
+import { keysToCamelCase, keysToSnakeCase } from "@/utils/helpers";
 
 import type { Settings } from "@/types";
 
@@ -9,7 +11,7 @@ export async function getSettings() {
     data,
     error,
   }: {
-    data: Settings | null;
+    data: Record<string, unknown> | null;
     error: PostgrestError | null;
   } = await supabase.from("settings").select("*").single();
 
@@ -18,14 +20,14 @@ export async function getSettings() {
     throw new Error("Settings could not be loaded");
   }
 
-  return data;
+  return data ? (keysToCamelCase(data) as unknown as Settings) : null;
 }
 
 // We expect a newSetting object that looks like {setting: newValue}
-export async function updateSetting(newSetting: Settings) {
+export async function updateSetting(newSetting: Partial<Settings>) {
   const { data, error } = await supabase
     .from("settings")
-    .update(newSetting)
+    .update(keysToSnakeCase(newSetting))
     // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
     .eq("id", 1)
     .single();
